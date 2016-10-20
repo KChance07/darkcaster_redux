a6a4bded786a86c86000e2a3067c4493702740f7
Kyle Chance <kylechance@gmail.com>
2016-10-20 18:49:14 -0400

    created middle, proc, and routers

---
+++ middleware/auth.js
@@ -0,0 +1,14 @@
+var mySecretPassphrase = process.env.PASS || require('..config.js').secretPassphrase;
+
+function authorize(request, response, next){
+  var secretPassphrase = request.headers.secret;
+  if(!secretPassphrase || secretPassphrase !== mySecretPassphrase){
+    response.status(403).json({
+      msg: 'You are still not authorized'
+    });
+  } else{
+      next();
+  }
+}
+
+module.exports = authorize;
